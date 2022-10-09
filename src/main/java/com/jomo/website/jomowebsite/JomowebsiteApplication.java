654088929b3617c36247393c7bdbc9bd60fa3a67
alexilie <sorin.alex.ilie@gmail.com>
2022-10-09 01:27:22 -0400

    Web Content loaded in static folder

---
+++ src/main/java/com/jomo/website/jomowebsite/JomowebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.jomo.website.jomowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JomowebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JomowebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jomo/website/jomowebsite/JomowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jomo.website.jomowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JomowebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
